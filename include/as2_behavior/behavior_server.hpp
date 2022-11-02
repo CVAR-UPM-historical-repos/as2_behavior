ec2fa257de222eae820753bc926d9a2a0506abd6
Miguel <miferco97@gmail.com>
2022-11-02 12:17:43 +0100

    first commit

---
+++ include/as2_behavior/behavior_server.hpp
@@ -0,0 +1,11 @@
+#ifndef AS2_NODE_TEMPLATE_HPP_
+#define AS2_NODE_TEMPLATE_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+
+class BehaviorServer : public rclcpp::Node {
+public:
+  BehaviorServer();
+};
+
+#endif  // AS2_NODE_TEMPLATE_HPP_
